2147012635034a1d8a6fdffb9fe129fa5ea290b1
Moitreyo <cmoitreyo@gmail.com>
2023-08-13 18:56:16 +0530

    Added ifLoggedIn method

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,21 +2,17 @@ const app = require("./app");
 const mongoose = require("mongoose");
 require("dotenv").config({ path: "./config.env" });
 
+// const cookie_parser = require("cookie-parser");
 
-const cookie_parser = require("cookie-parser");
+const Atlas =
+  "mongodb+srv://moitreyo:codinghabits365@cluster1.oivomfg.mongodb.net/readerInsect?retryWrites=true&w=majority";
 
-const Atlas = "mongodb+srv://moitreyo:codinghabits365@cluster1.oivomfg.mongodb.net/readerInsect?retryWrites=true&w=majority"
-
-
-app.use(cookie_parser());
+// app.use(cookie_parser());
 
 mongoose
-  .connect(
-    "mongodb://127.0.0.1:27017/Readect",
-    {
-      useNewUrlParser: true,
-    }
-  )
+  .connect(Atlas, {
+    useNewUrlParser: true,
+  })
   .then(() => {
     console.log("db connection successful");
   });
